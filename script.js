4f7692d87243593e34a52a97096895f851ea2b17
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-08-02 14:07:41 +0530

    modified:   index.html modified:   script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const cells =  document.querySelectorAll(".cell");
+const result = document.querySelector("#result");
+
+
+const win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [1, 4, 7],
+    [0, 4, 9],
+    [2, 4, 6],
+    [0, 3, 6],
+    [2, 5, 8]
+];
+
+// Add click to input
+// Test with X first
+
+function input() {
+    
+}
